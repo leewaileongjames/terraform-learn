5860098686b068e58f65104eba5d04adc0339780
leewaileongjames <leewljames@gmail.com>
2021-12-12 22:41:06 +0800

    new branch

--- main.tf
+++ main.tf
@@ -2,40 +2,127 @@ provider "aws" {
     region = "us-east-2"
 }
 
-variable "cidr_blocks" {
-    description = "cidr blocks and name tags for vpc and subnet"
-    default = [
-        {cidr_block = "10.0.0.0/16", name = "nana-dev-vpc"},
-        {cidr_block = "10.0.10.0/24", name = "nana-dev-subnet"}
-    ]
-    type = list(object({
-        cidr_block = string,
-        name = string
-    }))
-}
-
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
 variable avail_zone {} // Set using the TF_VAR_avail_zone env variable
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable docker_image {}
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name: var.cidr_blocks[0].name
+        Name: "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
-        Name: var.cidr_blocks[1].name
+        Name: "${var.env_prefix}-subnet-1"
     }
 }
 
-output "dev-vpc-id" {
-    value = aws_vpc.development-vpc.id
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
 }
 
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet.id
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-main-rtb"
+    }
+}
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+      from_port = 5000
+      to_port = 5000
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-default-sg"
+    }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["137112412989"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
+
+/* resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file("var.public_key_location")
+} */
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = "myAWSKP" // OR aws_key_pair.ssh-key.key_name
+
+    user_data = <<EOF
+#!/bin/bash
+sudo yum update -y && sudo yum install -y docker
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+newgrp docker
+docker run -p 5000:80 ${var.docker_image}
+EOF
+
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
 }
